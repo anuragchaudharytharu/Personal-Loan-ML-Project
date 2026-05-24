187c91dcb899a9133a93a59fcdef814062b59f36
Anurag Chaudhary Tharu <anuragchaudharytharu371@gmail.com>
2026-05-24 11:12:43 +0545

    project structure

--- template.py
+++ template.py
@@ -2,43 +2,57 @@ import os
 from pathlib import Path
 import logging
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.INFO, format="[%(asctime)s]: %(message)s")
 
 project_name = "mlproject"
+
 
 list_of_files = [
     f"src/{project_name}/__init__.py",
     f"src/{project_name}/components/__init__.py",
-    f"src/{project_name}/components/data_ingestion.py",
-    f"src/{project_name}/components/data_transformation.py",
-    f"src/{project_name}/components/model_trainer.py",
-    f"src/{project_name}/components/model_monitering.py",
-    f"src/{project_name}/pipelines/__init__.py",
-    f"src/{project_name}/pipelines/training_pipeline.py",
-    f"src/{project_name}/pipelines/prediction_pipeline.py",
-    f"src/{project_name}/exception.py",
-    f"src/{project_name}/logger.py",
-    f"src/{project_name}/utils.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/utils/common.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/entity/config_entity.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "params.yaml",
+    "schema.yaml",
+    "main.py",
     "app.py",
-    "Dockerfile",
     "requirements.txt",
     "setup.py",
+    "research/trials.ipynb",
+    "templates/index.html",
     "agenda.txt"
 ]
 
 
-for file_path in list_of_files:
-    file_path = Path(file_path)
-    file_directory, file_name = os.path.split(file_path)
+for filepath in list_of_files:
     
-    if file_directory != "":
-        os.makedirs(file_directory, exist_ok=True)
-        logging.info(f"Creating firectory: {file_directory} for the file {file_name}")   
-         
-    if (not os.path.exists(file_path)) or (os.path.getsize(file_path == 0)):
-        with open(file_path, 'w') as f:
+    # create file path for the list of files. Path() is used to make different os read the file like for windows, linux etc 
+    file_path = Path(filepath)
+    
+    # store file directory and file name by spliting the file path
+    file_dir, file_name = os.path.split(file_path)
+
+    if file_dir:
+        # make directory automatically if it doesnot exist
+        os.makedirs(file_dir, exist_ok=True)
+        logging.info(
+            f"Creating directory: {file_dir} for the file: {file_name}"
+        )
+        
+    # create file if not exists OR size of file is empty
+    if (not os.path.exists(file_path)) or (os.path.getsize(file_path) == 0):
+        # simply creates an EMPTY file.
+        with open(file_path, "w") as f:
             pass
         logging.info(f"Creating empty file: {file_path}")   
-         
+    
     else:
-        logging.info(f"{file_name} already exists.")
+        logging.info(f"{file_name} already exists")
+        
